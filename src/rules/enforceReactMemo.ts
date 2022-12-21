c8b0d30171903d01cd348e2334039aa2d5c4ecb7
Anshul Sanghi <anshul@anshulsanghi.tech>
2022-12-21 09:59:05 +0000

    Fix Rules And Make Them Work With Any Component Declaration In A File

--- src/rules/enforceReactMemo.ts
+++ src/rules/enforceReactMemo.ts
@@ -24,103 +24,136 @@ const enforceReactMemo = createRule({
     defaultOptions: [{ ignoreHooks: [] as string[] }],
     create(context) {
         return {
-            ExportNamedDeclaration(node) {
-                if (
-                    node.declaration?.type ===
-                    AST_NODE_TYPES.VariableDeclaration
-                ) {
-                    const declaration = node.declaration.declarations[0];
-                    if (
-                        declaration.type === AST_NODE_TYPES.VariableDeclarator
-                    ) {
-                        const init = declaration.init;
-                        if (!init) {
-                            // Not a component
-                            return;
-                        }
+            // ExportDefaultDeclaration(node) {
+            //     if (node.declaration.type === AST_NODE_TYPES.Identifier) {
+            //         const identifier = node.declaration.name;
 
-                        const name =
-                            declaration.id.type === AST_NODE_TYPES.Identifier
-                                ? declaration.id.name
-                                : undefined;
+            //         if (
+            //             !identifier ||
+            //             // Shallow checking for non-component declaration
+            //             identifier.charAt(0) !==
+            //                 identifier.charAt(0).toUpperCase()
+            //         ) {
+            //             return;
+            //         }
 
-                        if (
-                            init.type ===
-                                AST_NODE_TYPES.ArrowFunctionExpression &&
-                            name &&
-                            // Shallow checking for component declaration
-                            name.charAt(0) === name.charAt(0).toUpperCase()
-                        ) {
-                            // TODO: Only report if component
-                            context.report({
-                                node,
-                                messageId: 'general',
-                            });
-                            return;
-                        }
+            //         const declarationNode = (
+            //             (node.parent as any).body as TSESTree.BaseNode[]
+            //         ).find(item => {
+            //             if (item.type === AST_NODE_TYPES.VariableDeclaration) {
+            //                 const declaration = (
+            //                     item as TSESTree.VariableDeclaration
+            //                 ).declarations[0];
 
-                        if (init.type === AST_NODE_TYPES.CallExpression) {
-                            const callee = init.callee;
-                            if (callee.type === AST_NODE_TYPES.Identifier) {
-                                if (callee.name !== 'memo') {
-                                    context.report({
-                                        node,
-                                        messageId: 'general',
-                                    });
-                                    return;
-                                }
-                            }
-                        }
+            //                 if (
+            //                     declaration.type ===
+            //                     AST_NODE_TYPES.VariableDeclarator
+            //                 ) {
+            //                     const id = declaration.id;
+            //                     if (id.type === AST_NODE_TYPES.Identifier) {
+            //                         return id.name === identifier;
+            //                     }
+            //                 }
+            //             }
+            //         }) as TSESTree.VariableDeclaration | undefined;
+
+            //         if (!declarationNode) {
+            //             return;
+            //         }
+
+            //         const init = declarationNode.declarations[0].init;
+
+            //         if (!init) {
+            //             return;
+            //         }
+
+            //         if (init.type === AST_NODE_TYPES.ArrowFunctionExpression) {
+            //             context.report({
+            //                 node: declarationNode,
+            //                 messageId: 'general',
+            //             });
+            //             return;
+            //         }
+
+            //         if (init.type === AST_NODE_TYPES.CallExpression) {
+            //             const callee = (init as CallExpression).callee;
+
+            //             if (callee.type === AST_NODE_TYPES.Identifier) {
+            //                 if ((callee as Identifier).name !== 'memo') {
+            //                     context.report({
+            //                         node,
+            //                         messageId: 'general',
+            //                     });
+            //                     return;
+            //                 }
+            //             }
+            //         }
+            //     }
+            // },
+            VariableDeclaration(node) {
+                const declaration = node.declarations[0];
+
+                if (declaration.type === AST_NODE_TYPES.VariableDeclarator) {
+                    const init = declaration.init;
+                    if (!init) {
+                        // Not a component
+                        return;
                     }
-                }
-            },
-            ExportDefaultDeclaration(node) {
-                if (node.declaration.type === AST_NODE_TYPES.Identifier) {
-                    const identifier = node.declaration.name;
 
-                    const declarationNode = (
+                    const name =
+                        declaration.id.type === AST_NODE_TYPES.Identifier
+                            ? declaration.id.name
+                            : undefined;
+
+                    const isExportMemoized = (
                         (node.parent as any).body as TSESTree.BaseNode[]
-                    ).find(item => {
-                        if (item.type === AST_NODE_TYPES.VariableDeclaration) {
-                            const declaration = (
-                                item as TSESTree.VariableDeclaration
-                            ).declarations[0];
+                    )?.some(item => {
+                        if (
+                            item.type ===
+                            AST_NODE_TYPES.ExportDefaultDeclaration
+                        ) {
+                            const exportDeclaration = (
+                                item as TSESTree.ExportDefaultDeclaration
+                            ).declaration;
 
                             if (
-                                declaration.type ===
-                                AST_NODE_TYPES.VariableDeclarator
+                                exportDeclaration &&
+                                exportDeclaration.type ===
+                                    AST_NODE_TYPES.CallExpression
                             ) {
-                                const id = declaration.id;
-                                if (id.type === AST_NODE_TYPES.Identifier) {
-                                    return id.name === identifier;
+                                const callee = exportDeclaration.callee;
+                                if (
+                                    callee.type === AST_NODE_TYPES.Identifier &&
+                                    callee.name === 'memo'
+                                ) {
+                                    return true;
                                 }
                             }
                         }
-                    }) as TSESTree.VariableDeclaration | undefined;
 
-                    if (!declarationNode) {
-                        return;
-                    }
+                        return false;
+                    });
 
-                    const init = declarationNode.declarations[0].init;
-
-                    if (!init) {
-                        return;
-                    }
-
-                    if (init.type === AST_NODE_TYPES.ArrowFunctionExpression) {
+                    if (
+                        init.type === AST_NODE_TYPES.ArrowFunctionExpression &&
+                        name &&
+                        // Shallow checking for component declaration
+                        name.charAt(0) === name.charAt(0).toUpperCase() &&
+                        !isExportMemoized
+                    ) {
+                        // TODO: Only report if component
                         context.report({
-                            node: declarationNode,
+                            node,
                             messageId: 'general',
                         });
+
                         return;
                     }
 
                     if (init.type === AST_NODE_TYPES.CallExpression) {
-                        const callee = (init as CallExpression).callee;
-
+                        const callee = init.callee;
                         if (callee.type === AST_NODE_TYPES.Identifier) {
-                            if ((callee as Identifier).name !== 'memo') {
+                            if (callee.name !== 'memo') {
                                 context.report({
                                     node,
                                     messageId: 'general',
--- src/tests/rules/enforeReactMemo.spec.ts
+++ src/tests/rules/enforeReactMemo.spec.ts
@@ -14,13 +14,94 @@ ruleTester.run('enforce-react-memo', enforceReactMemo, {
     valid: [
         {
             code: `
-                const component = () => {
+                const Component = () => {
                     return <div>Something</div>;
                 };
 
-                export default component;
+                export default memo(component);
+            `,
+        },
+        {
+            code: `
+                const Component = memo(() => {
+                    return <div>Something</div>;
+                });
+
+                export default Component;
+            `,
+        },
+        {
+            code: `
+                export const Component = memo(() => {
+                    return <div>Something</div>;
+                });
+            `,
+        },
+        {
+            code: `
+                export const Component = memo(forwardRef(() => {
+                    return <div>Something</div>;
+                }));
+            `,
+        },
+        {
+            code: `
+                export const nonComponentFunction = () => {
+                    return <div>Something</div>;
+                };
+            `,
+        },
+        {
+            code: `
+                const nonComponentFunction = () => {
+                    return <div>Something</div>;
+                };
+
+                export default nonComponentFunction;
+            `,
+        },
+        {
+            code: `
+                const NonExportedComponent = memo(() => {
+                    return <div>Something</div>;
+                });
             `,
         },
     ],
-    invalid: [],
+    invalid: [
+        {
+            code: `
+                const Component = () => {
+                    return <div>Something</div>;
+                };
+
+                export default Component;
+            `,
+            errors: [{ messageId: 'general' }],
+        },
+        {
+            code: `
+                export const Component = () => {
+                    return <div>Something</div>;
+                };
+            `,
+            errors: [{ messageId: 'general' }],
+        },
+        {
+            code: `
+                export const Component = forwardRef(() => {
+                    return <div>Something</div>;
+                });
+            `,
+            errors: [{ messageId: 'general' }],
+        },
+        {
+            code: `
+                const NonExportedComponent = () => {
+                    return <div>Something</div>;
+                };
+            `,
+            errors: [{ messageId: 'general' }],
+        },
+    ],
 });
